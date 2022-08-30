03604504a3bd63d77108ec58209a1b0f68d1f794
Tahir Günsen <tgunsen@pm.me>
2022-08-30 16:30:37 +0200

    routes, authentication skipped for parties-index-show, home page added button and slogan

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,13 @@
 Rails.application.routes.draw do
-  get 'reviews/index'
-  get 'reviews/show'
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'chatrooms/index'
-  get 'chatrooms/show'
-  get 'chatrooms/new'
-  get 'chatrooms/create'
-  get 'requests/index'
-  get 'requests/show'
-  get 'requests/new'
-  get 'requests/create'
-  get 'requests/edit'
-  get 'requests/update'
-  get 'requests/destroy'
-  get 'favorites/index'
-  get 'favorites/show'
-  get 'favorites/new'
-  get 'favorites/create'
-  get 'favorites/destroy'
-  get 'parties/index'
-  get 'parties/show'
-  get 'parties/new'
-  get 'parties/create'
-  get 'parties/edit'
-  get 'parties/update'
-  get 'parties/destroy'
-  devise_for :users
-  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "pages#home"
+  devise_for :users
+  resources :parties
+  resources :requests
+  resources :reviews
+  resources :favorites
+  resources :chatrooms
 end
