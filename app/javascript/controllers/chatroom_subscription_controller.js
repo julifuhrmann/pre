cdbcd17cd8120b46390e51c491e0ec6c2c49f6ea
Juliana Fuhrmann <julifuhrmann@hotmail.com>
2022-09-09 12:35:48 +0200

    change

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -3,8 +3,10 @@ import { createConsumer } from "@rails/actioncable"
 
 // Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
-  static values = { chatroomId: Number }
   static targets = ["messages"]
+  static values = {
+    chatroomId: Number, currentUserId: Number
+  }
 
   connect() {
     this.channel = createConsumer().subscriptions.create(
@@ -14,8 +16,29 @@ export default class extends Controller {
     console.log(`Subscribed to chatroom with the id ${this.chatroomIdValue}.`)
   }
 
+  #justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
+  }
+
+  #userStyleClass(currentUserIsSender) {
+    console.log(currentUserIsSender ? "sender-style" : "receiver-style");
+    return currentUserIsSender ? "sender-style" : "receiver-style"
+  }
+
+  #buildMessageElement(currentUserIsSender, message) {
+    return `<div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+    <div class="${this.#userStyleClass(currentUserIsSender)}">
+      ${message}
+    </div>
+    </div>`
+  }
+
   #insertMessageAndScrollDown(data) {
-    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    const currentUserIsSender = this.currentUserIdValue === data.sender_id
+    console.log(data.sender_id);
+    console.log(this.currentUserIdValue);
+    const messageElement = this.#buildMessageElement(currentUserIsSender, data.message)
+    this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
   }
 
